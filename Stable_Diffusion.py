704bab3e915083023ab93b6ebcb770552ffbf6b7
19375092杜雨桐 <duuong@yeah.net>
2024-04-30 14:01:26 +0800

    hahah

--- Stable_Diffusion.py
+++ Stable_Diffusion.py
@@ -9,17 +9,17 @@ class Diffusion_Generator(Generator):
         from diffusers import StableDiffusionImg2ImgPipeline
         from huggingface_hub import login
         login(token=access_token)
-        cache_dir = save_dir if save_dir != None else './Stable_Diffusion/cache'
+        cache_dir = save_dir if save_dir != None else './Stable_Diffusion/cache/'
         if version == 'Stable Diffusion v1.4':
             model_path = "CompVis/stable-diffusion-v1-4"  
         if version == "Small Stable Diffusion v0":
-            model_path = "OFA-Sys/small-stable-diffusion-v0/"
+            model_path = "OFA-Sys/small-stable-diffusion-v0"
         self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
                                             model_path,
                                             revision="fp16", 
                                             torch_dtype=torch.float16,
                                             use_auth_token=True,
-                                            cache_dir=cache_dir,
+                                            cache_dir=cache_dir + model_path,
                                         )
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.pipe = self.pipe.to(device)
